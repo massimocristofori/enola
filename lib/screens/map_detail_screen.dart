45af8f1f4ca232ed42a31689574b4d966066d776
massimocristofori <massimo.cristofori@gmail.com>
2026-05-07 12:47:02 +0200

    Remove unused flutter_animate import

--- lib/screens/map_detail_screen.dart
+++ lib/screens/map_detail_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_animate/flutter_animate.dart';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:enola/models/riddle.dart';
