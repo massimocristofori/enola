98f78d87a852a46fe81ade9c30b810f7ace42f98
massimocristofori <massimo.cristofori@gmail.com>
2026-05-07 12:46:48 +0200

    Update create_map_screen.dart

--- lib/screens/create_map_screen.dart
+++ lib/screens/create_map_screen.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter_animate/flutter_animate.dart';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:enola/models/riddle.dart';
