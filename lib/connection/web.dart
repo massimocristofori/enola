be8a8a52be58d7f70e0422125ae8d44e1ba403fb
massimocristofori <massimo.cristofori@gmail.com>
2026-05-08 09:06:32 +0200

    Update web.dart

--- lib/connection/web.dart
+++ lib/connection/web.dart
@@ -1,13 +1,6 @@
 import 'package:drift/drift.dart';
-import 'package:drift/wasm.dart';
+import 'package:drift/web.dart';
 
 QueryExecutor connect() {
-  return DatabaseConnection.delayed(Future(() async {
-    final result = await WasmDatabase.open(
-      databaseName: 'enola_db',
-      sqlite3Uri: Uri.parse('sqlite3.wasm'),
-      driftWorkerUri: Uri.parse('drift_worker.js'),
-    );
-    return result.resolvedExecutor;
-  }));
+  return WebDatabase('enola_db');
 }
