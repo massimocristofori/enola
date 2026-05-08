c9faf8876a50e1e75bbfa46a58b5bb1b2dd30d8a
Cirrus CI <support@cirruslabs.org>
2026-05-08 07:33:15 +0000

    .

--- lib/connection/web.dart
+++ lib/connection/web.dart
@@ -6,7 +6,7 @@ QueryExecutor connect() {
     final result = await WasmDatabase.open(
       databaseName: 'enola_db',
       sqlite3Uri: Uri.parse('sqlite3.wasm'),
-      driftWorkerUri: Uri.parse('drift_worker.js'),
+      driftWorkerUri: Uri.parse('drift_worker.dart.js'),
     );
     return result.resolvedExecutor;
   }));
