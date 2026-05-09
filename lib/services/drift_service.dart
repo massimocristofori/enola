f2f35a5dc31c3565dbd150071966bef65e2fcfc7
massimocristofori <massimo.cristofori@gmail.com>
2026-05-09 07:52:21 +0200

    Update drift_service.dart

--- lib/services/drift_service.dart
+++ lib/services/drift_service.dart
@@ -3,6 +3,8 @@ import 'package:drift/drift.dart';
 import 'package:enola/database/database.dart';
 import 'package:enola/connection/connection.dart' as impl;
 import 'package:uuid/uuid.dart';
+import 'package:enola/database/schema_utils.dart';
+
 
 class DriftService {
   // Singleton pattern
