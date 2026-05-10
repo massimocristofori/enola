83cd4f3f6510911a4902e1df4769c15a9759f4cf
massimocristofori <massimo.cristofori@gmail.com>
2026-05-10 05:45:13 +0200

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,7 +3,6 @@ import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:intl/intl.dart';
 
 import 'package:enola/database/database.dart';
 import 'package:enola/providers/map_providers.dart';
