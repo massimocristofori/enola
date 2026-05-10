11fe6444e71f3485b0f36aab07bfc1a0dfd09f2e
massimocristofori <massimo.cristofori@gmail.com>
2026-05-10 05:46:52 +0200

    Update drift_service.dart

--- lib/services/drift_service.dart
+++ lib/services/drift_service.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:typed_data';
 import 'package:drift/drift.dart';
 import 'package:enola/database/database.dart';
 import 'package:enola/connection/connection.dart' as impl;
