2255274debf34253640f999250a615eb09503b5c
massimocristofori <massimo.cristofori@gmail.com>
2026-05-13 11:03:39 +0200

    Update gemini_service.dart

--- lib/services/gemini_service.dart
+++ lib/services/gemini_service.dart
@@ -23,7 +23,7 @@ class GeminiService {
     assert(apiKey != null && apiKey!.isNotEmpty,
         'GeminiService: apiKey not set');
     return GenerativeModel(
-      model: 'gemini-2.0-flash-lite',
+      model: 'gemini-1.5-flash',
       apiKey: apiKey!,
     );
   }
