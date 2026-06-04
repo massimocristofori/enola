8c99beac1408e70bbc42e3fd7e8f9b69bd8c1582
massimocristofori <massimo.cristofori@gmail.com>
2026-06-04 10:20:38 +0200

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -352,7 +352,7 @@ class _RankRibbonOverlay extends StatelessWidget {
           gradient: const LinearGradient(
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
-            colors: [Color(0xFFF9CF58).withAlpha(230), Color(0xFFF6B700).withAlpha(230)],
+            colors: [const Color(0xFFF9CF58).withAlpha(230), const Color(0xFFF6B700).withAlpha(230)],
           ),
           boxShadow: [
             BoxShadow(
